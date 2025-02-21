f7b7c7b396b16c76c53f53b699ccef8e8f34013f
alnah <alexis.nahan@gmail.com>
2025-02-21 16:53:51 -0300

    feat: generate pages recursively

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 from copytree import copytree
-from page import generate_page
+from page import generate_pages_recursive
 
 
 def main() -> None:
     copytree("static", "public")
-    generate_page("template.html", "content/index.md", "public/index.html")
+    generate_pages_recursive("template.html", "content/", "public/")
 
 
 if __name__ == "__main__":
--- src/page.py
+++ src/page.py
@@ -1,6 +1,7 @@
-from logger import get_logger
-from converter import markdown_text_to_html_node
 import os
+
+from converter import markdown_text_to_html_node
+from logger import get_logger
 
 
 class MarkdownTitleError(Exception):
@@ -43,10 +44,31 @@ def write_file(file_path: str, data: str) -> None:
 
 def generate_page(template_path: str, src_path: str, dst_path: str) -> None:
     logger = get_logger()
-    template, markdown = read_file(template_path), read_file(src_path)
-    html = markdown_text_to_html_node(markdown).to_html()
-    title = extract_title(markdown)
-    page = template.replace("{{ Title }}", title).replace("{{ Content }}", html)
-    logger.info(html)
-    write_file(dst_path, page)
+    try:
+        template, markdown = read_file(template_path), read_file(src_path)
+        html = markdown_text_to_html_node(markdown).to_html()
+        title = extract_title(markdown)
+        page = template.replace("{{ Title }}", title).replace("{{ Content }}", html)
+        if src_path.endswith(".md"):
+            dst_path = dst_path.rsplit(".", 1)[0] + ".html"
+            write_file(dst_path, page)
+    except Exception as e:
+        logger.info(e)
     logger.info(f'Generated page: from "{src_path}" to "{dst_path}"')
+
+
+def generate_pages_recursive(
+    template_path: str,
+    current_src: str,
+    current_dst: str,
+) -> None:
+    logger = get_logger()
+    os.makedirs(current_dst, exist_ok=True)
+    for branch in os.listdir(current_src):
+        src_path = os.path.join(current_src, branch)
+        dst_path = os.path.join(current_dst, branch)
+        if os.path.isfile(src_path):
+            generate_page(template_path, src_path, dst_path)
+        elif os.path.isdir(src_path):
+            generate_pages_recursive(template_path, src_path, dst_path)
+    logger.info(f'Generated all pages: from "{current_src}" to {current_dst}"')
